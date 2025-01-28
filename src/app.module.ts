20dcdce474a7ce3cc2b91d2e907ee3888a809624
Yuldoshova <ozoda8171@gmail.com>
2025-01-28 12:47:26 +0500

    main and app-module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,103 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { JwtModule } from '@nestjs/jwt';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { APP_GUARD } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { RedisCustomModule } from '@redis';
+import { appConfig, dbConfig, emailConfig, jwtConfig } from '@config';
+import { CheckAuthGuard, CheckRoleGuard } from '@guards';
+import { AuthModule, Brand, BrandModule, Category, CategoryModule, User, UserModule } from '@modules';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 30000,
+      limit: 300,
+    }]),
+    ConfigModule.forRoot({
+      load: [appConfig, dbConfig, jwtConfig, emailConfig],
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    ServeStaticModule.forRoot({
+      serveRoot: '/uploads',
+      rootPath: './uploads',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        port: configService.get<number>('dbConfig.port'),
+        host: configService.get<string>('dbConfig.host'),
+        username: configService.get<string>('dbConfig.user'),
+        password: configService.get<string>('dbConfig.password'),
+        database: configService.get<string>('dbConfig.dbName'),
+        entities: [User, Brand, Category],
+        autoLoadEntities: true,
+        synchronize: true,
+        // logging: true
+      }),
+      inject: [ConfigService]
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        options: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+    JwtModule.register({
+      secret: 'my secret',
+      global: true,
+      signOptions: {
+        expiresIn: 60 * 15,
+      },
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('emailConfig.host'),
+          port: configService.get<number>('emailConfig.port'),
+          secure: false,
+          auth: {
+            user: configService.get<string>('emailConfig.username'),
+            pass: configService.get<string>('emailConfig.password'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    RedisCustomModule,
+    UserModule,
+    AuthModule,
+    BrandModule,
+    CategoryModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    {
+      useClass: CheckAuthGuard,
+      provide: APP_GUARD,
+    },
+    {
+      useClass: CheckRoleGuard,
+      provide: APP_GUARD,
+    },
+  ],
 })
-export class AppModule {}
+
+export class AppModule { }
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1"
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ASHYO SHOP')
+    .setDescription('The ashyo backend apis')
+    .setVersion('1.0')
+    .addTag('APIS')
+    .build();
+    
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  const appConfig = app.get(ConfigService)
+
+  await app.listen(
+    appConfig.get<number>('appConfig.port'),
+    appConfig.get<string>('appConfig.host'), () => {
+      console.log(`Server running port on ${appConfig.get<number>('appConfig.port')}`)
+    });
 }
 bootstrap();
+
+
